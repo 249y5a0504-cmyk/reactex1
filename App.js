49d993569f8291dbc38731bb569550f5ade8f939
249y5a0504-cmyk <249y5a0504@ksrmce.ac.in>
2026-03-31 14:15:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,164 @@
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
+import React, { Component } from "react";
+
+class CounterClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Class Component Counter</h2>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.increment}>Increment</button>
+            </div>
+        );
+    }
+}
+
+export default CounterClass;
+import React from "react";
+import CounterFunction from "./components/CounterFunction";
+import ClickHandler from "./components/ClickHandler";
+
+function App() {
+  return (
+    <div>
+      <h1>React Counter Example</h1>
+      <ClickHandler />
+    </div>
+  );
+}
+
+export default App; 
+
+import ConditionalRender from "./components/ConditionalRender";
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      <ConditionalRender />
+    </div>
+  );
+}
+
+export default App; 
+
+import react from "react";
+import StringLiteral from "./components/StringLiteral";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My App</h1>
+      <StringLiteral />
+    </div>
+  );
+}
+
+export default App; 
+
+import React from "react";
+import CounterUseState from "./components/CounterUseState";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <CounterUseState />
+    </div>
+  );
+}
+
+export default App; 
+
+import React from "react";
+import FetchData from "./components/FetchData";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FetchData</h1>
+      <FetchData />
+    </div>
+  );
+}
+
+export default App; 
+
+import React from "react";
+import Parent from "./components/Parent";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Parent </h1>
+      <Parent/>
+    </div>
+  );
+}
+
+export default App; */
+
+/*import React from "react";
+import SimpleForm from "./components/SimpleForm";
+function App() {
+    return (
+        <div>
+            <h1>Simple Form App</h1>
+            <SimpleForm />
+        </div>
+    );
+}
+
+export default App;*/
+
+import React from "react";
+import ListRender from "./components/ListRender";
+function App() {
+    return (
+        <div>
+            <h1>ListRender</h1>
+            <SimpleForm />
+        </div>
+    );
+}
+
+export default App;
+
